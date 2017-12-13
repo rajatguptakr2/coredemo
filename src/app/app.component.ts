148fe30bf75c768cc77a04e143ab36afe4cfeddf
Rajat Gupta <rajat@tekserver.com>
2017-12-13 17:16:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { IProduct } from './products';
+import { ProductService } from './products.service';
+// import { appService } from 'app.service';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+
+  name = 'Angular';
+  DefaultData:string = 'Test';
+  status:boolean = true;
+
+  appList:any[] = [
+    {
+      "ID":"1",
+      "Name":"One"
+    },
+    {
+      "ID":"2",
+      "Name":"Two"
+    },
+  ]
+
+
+}
